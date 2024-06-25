a5e5d629674d78ca12fd0eb8cf1e89d927228cf4
juliacheperis13 <35917877+juliacheperis13@users.noreply.github.com>
2024-06-25 12:51:27 +0000

    Deploying to gh-pages from @ juliacheperis13/goit-advancedjs-hw-04@961a62897c1fcacbff6e436923e1215e48c3c282 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,16 +1,16 @@
-import{i as f,a as v}from"./assets/vendor-ae6d56ab.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const c of s.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&a(c)}).observe(document,{childList:!0,subtree:!0});function o(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function a(t){if(t.ep)return;t.ep=!0;const s=o(t);fetch(t.href,s)}})();const m={hidden:"hidden"},l=(r,e)=>{e?r.classList.add(m.hidden):r.classList.remove(m.hidden)},n=(r,e)=>{e?f.error({title:"Error",message:r,position:"topRight"}):f.success({title:"Success",message:r,position:"topRight"})};document.querySelector(".search-button");const u=document.querySelector(".load-more-button"),h=document.querySelector(".search-form"),d=document.querySelector(".gallery"),P="44510469-6a54537c1d88a141b8cb4f82a",L="https://pixabay.com/api/",i={currentPage:0,itemsPerPage:40};h.addEventListener("submit",b);u.addEventListener("click",w);function g(){return h.elements.searchQuery.value.trim()}async function b(r){r.preventDefault(),i.currentPage=1;const e=g();if(d.innerHTML="",l(u,!0),!e){n("Please fill in search query!",!0);return}try{const o=await p(e);if(o.hits.length)n("Images found!",!1),l(u,!1),d.insertAdjacentHTML("beforeend",y(o.hits));else{n("No images found!",!0),l(u,!0);return}}catch{n("Something went wrong!",!0)}}async function p(r){const e=new URLSearchParams({page:i.currentPage,per_page:i.itemsPerPage,image_type:"photo",orientation:"horizontal",safesearch:"true",key:P,q:r});try{return(await v.get(`${L}?${e}`)).data}catch{n("Something went wrong!",!0)}}async function w(){i.currentPage=i.currentPage+1;const r=g(),e=await p(r);i.currentPage*i.itemsPerPage>=Math.min(e.total,e.totalHits)&&S(),d.insertAdjacentHTML("beforeend",y(e.hits))}function S(){l(u,!0),d.children.length>0&&n("There are no more results!",!0)}function y(r){return r.map(({previewURL:e,tags:o,likes:a,views:t,comments:s,downloads:c})=>`
+import{i as f,a as P}from"./assets/vendor-ae6d56ab.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))l(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const c of o.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&l(c)}).observe(document,{childList:!0,subtree:!0});function s(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function l(t){if(t.ep)return;t.ep=!0;const o=s(t);fetch(t.href,o)}})();const h={hidden:"hidden"},u=(r,e)=>{e?r.classList.add(h.hidden):r.classList.remove(h.hidden)},n=(r,e)=>{e?f.error({title:"Error",message:r,position:"topRight"}):f.success({title:"Success",message:r,position:"topRight"})},a=document.querySelector(".load-more-button"),m=document.querySelector(".search-form"),d=document.querySelector(".gallery"),v="44510469-6a54537c1d88a141b8cb4f82a",L="https://pixabay.com/api/",i={currentPage:0,itemsPerPage:40};m.addEventListener("submit",b);a.addEventListener("click",w);function g(){return m.elements.searchQuery.value.trim()}async function b(r){r.preventDefault(),i.currentPage=1;const e=g();if(d.innerHTML="",u(a,!0),!e){n("Please fill in search query!",!0);return}try{const s=await p(e);if(s.hits.length)n(`Hooray! We found ${s.totalHits} images.`,!1),i.currentPage*i.itemsPerPage>=Math.min(s.total,s.totalHits)?u(a,!0):u(a,!1),d.insertAdjacentHTML("beforeend",y(s.hits));else{n("Sorry, there are no images matching your search query. Please try again.",!0),u(a,!0);return}}catch{n("Something went wrong!",!0)}}async function p(r){const e=new URLSearchParams({page:i.currentPage,per_page:i.itemsPerPage,image_type:"photo",orientation:"horizontal",safesearch:"true",key:v,q:r}),{data:s}=await P.get(`${L}?${e}`);return s}async function w(){try{i.currentPage=i.currentPage+1;const r=g(),e=await p(r);i.currentPage*i.itemsPerPage>=Math.min(e.total,e.totalHits)&&S(),d.insertAdjacentHTML("beforeend",y(e.hits))}catch{n("Something went wrong!",!0)}}function S(){u(a,!0),d.children.length>0&&n("We're sorry, but you've reached the end of search results.",!0)}function y(r){return r.map(({previewURL:e,tags:s,likes:l,views:t,comments:o,downloads:c})=>`
    <div class="photo-card">
   <img
     class="gallery-img"
     src="${e}"
-    alt="${o}"
+    alt="${s}"
     width="240"
     height="140"
   />
   <div class="info">
     <div class="info-item">
       <h3>Likes</h3>
-      <p>${a}</p>
+      <p>${l}</p>
     </div>
     <div class="info-item">
       <h3>Views</h3>
@@ -18,7 +18,7 @@ import{i as f,a as v}from"./assets/vendor-ae6d56ab.js";(function(){const e=docum
     </div>
     <div class="info-item">
       <h3>Comments</h3>
-      <p>${s}</p>
+      <p>${o}</p>
     </div>
     <div class="info-item">
       <h3>Downloads</h3>
